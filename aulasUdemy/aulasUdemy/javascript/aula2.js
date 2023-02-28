2738b901a837d020a8e5bf22436cde319116fd0e
lucasrafaellima <87723096+lucasrafaellima@users.noreply.github.com>
2023-02-28 11:31:16 -0300

    new files

---
+++ aulasUdemy/aulasUdemy/javascript/aula2.js
@@ -0,0 +1,11 @@
+var n = 25;
+var n1 = 10;
+
+document.write(n);
+document.write("<br>");
+document.write(n1);
+
+alert(n + " + " + n1 + " = " + (n + n1));
+alert(n + " - " + n1 + " = " + (n - n1));
+alert(n + " * " + n1 + " = " + (n * n1));
+alert(n + " / " + n1 + " = " + (n / n1));
