5bb39b4231b259250eeb85f6870cbd34fe6af2aa
lucasrafaellima <87723096+lucasrafaellima@users.noreply.github.com>
2023-04-12 11:10:17 -0300

    design

--- atividadeIfalJs/proWeb.js
+++ atividadeIfalJs/proWeb.js
@@ -1,4 +1,9 @@
 let firstName = ""
+let surname = ""
+let cpf = ""
+let phoneNumber = ""
+let datBirth = ""
+let Address = ""
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -8,5 +13,14 @@ form.addEventListener("submit", (e) => {
 
 function capturar() {
     firstName = document.getElementById('firstName').value
-    document.getElementById('show').innerHTML = firstName
+    surname = document.getElementById('surname').value
+    cpf = document.getElementById('cpf').value
+    phoneNumber = document.getElementById('phoneNumber').value
+    dateBirth = document.getElementById('dateBirth').value
+    Address = document.getElementById('Address').value
+
+
+    if (firstName != "" && surname != "" && cpf != "" && phoneNumber != "" && dateBirth != "" && Address != "") {
+        document.getElementById('show').innerHTML = `tudo ok`
+    }
 }
---
+++ atividadeIfalJs/testePHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>teste PHP</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Lucas Lima">
+        <meta name="Description" content="pagina pra integrar banco de dados">
+        <meta name="keyword" content="html5 com db">
+    </head>
+    <body>
+        <h1>Cadastrar usuário</h1>
+        <form method="POST" action="teste.php">
+            <label>Nome: </label>
+            <input type="text" name="nome" placeholder="digite o nome completo"><br><br>
+            <label>Email: </label>
+            <input type="email" name="email" placeholder="digite o seu melhor email"><br><br>
+            <input type="submit" value="Cadastrar">
+        </form>
+    </body>
+</html>
